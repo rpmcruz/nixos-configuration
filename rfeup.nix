794346e6f6c9a12a9fa5f318cb9f96d0a12b557c
Ricardo Cruz <ricardo.pdm.cruz@gmail.com>
2025-12-18 16:46:11 +0000

    Fixed error due to bash not found.

--- rfeup.nix
+++ rfeup.nix
@@ -15,7 +15,7 @@ home-manager.users.rpcruz.systemd.user.services.update_fp = {
   Service = {
     Type = "oneshot";
     WorkingDirectory = "/home/rpcruz/projs/FP-Admin/moodle-api";
-    ExecStart = "/usr/bin/env bash -lc 'source venv/bin/activate && ./leaderboard_update.sh'";
+    ExecStart = "${pkgs.bash}/bin/bash -lc 'source venv/bin/activate && ./leaderboard_update.sh'";
   };
 };
 home-manager.users.rpcruz.systemd.user.timers.update_fp = {
