89b897e9e58161960dd6b17be4fb505650f7ccbf
Ricardo Cruz <ricardo.pdm.cruz@gmail.com>
2025-12-19 18:21:44 +0000

    Allow service to take a long time to run.

--- rfeup.nix
+++ rfeup.nix
@@ -22,6 +22,7 @@ home-manager.users.rpcruz.systemd.user.services.update_fp = {
     Type = "oneshot";
     WorkingDirectory = "/home/rpcruz/projs/FP-Admin/moodle-api";
     ExecStart = "${pkgs.bash}/bin/bash -lc 'source venv/bin/activate && ./leaderboard_update.sh'";
+    TimeoutStartSec = "60min";
   };
 };
 home-manager.users.rpcruz.systemd.user.timers.update_fp = {
