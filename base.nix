df68283f5586360e8cf7f57ba6a3fa837555ab28
Ricardo Cruz <ricardo.pdm.cruz@gmail.com>
2026-01-02 18:14:41 +0000

    xournalpp

--- base.nix
+++ base.nix
@@ -77,7 +77,7 @@ environment.systemPackages = with pkgs; [
   pinta
   transmission_4-gtk
   gummi texliveFull
-  pdfarranger
+  xournalpp pdfarranger
   # Nix comes with many python packages, but pip has more packages
   # we can just do "python3 -m venv name" and then install packages there
   python3
